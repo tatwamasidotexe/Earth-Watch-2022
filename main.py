036d8056397fbbf5ae92eae74e098c2d6cf5eb18
Ayush Mishra <46701430+Arn0dian@users.noreply.github.com>
2022-02-18 20:11:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+def electricCalc(amused,nf):
+    return((amused*0.95)/nf)
+
+def vehicleCalc(distance,mil,nf,type):
+    if type.lower()=="p":
+        return(((distance/mil)*2.392)/nf)
+    else:
+        return(((distance/mil)*2.640)/nf)      
+
+def flightCalc(inthours,dmhours):
+    total = 0
+    total+=((dmhours*90)/75)
+    total+=((inthours*90)/416)
+    return(total)
+
+def iceCalc(carbon):
+    return(carbon*650)
+
 from tkinter import *
 
 root = Tk()
